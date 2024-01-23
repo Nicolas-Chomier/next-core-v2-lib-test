290eb66806a580a19e25f34cf26972269124841a
Nicolas Chomier <nicolas.chomier@protonmail.com>
2024-01-23 23:21:23 +0100

    Nettoyage et optimisation pour le test des composants

--- app/pages/(Landing page)/page.tsx
+++ app/pages/(Landing page)/page.tsx
@@ -1,91 +1,49 @@
 'use client';
 // React core
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 // External modules / Third-party libraries
-
+import { nanoid } from 'nanoid';
+import { ZodType, z } from 'zod';
+import { zodResolver } from '@hookform/resolvers/zod/dist/zod.js';
 // Local components
-import { PieChartEx } from '@/app/components/showRoomTest/PieChart';
-import { AreaChartEx } from '@/app/components/showRoomTest/AreaChart';
-import { ShowRoomForm } from '@/app/components/showRoomTest/ShowRoomForm';
-import { UserProfile } from '@/Library/UserProfile/UserProfile';
+import { DatePicker } from '@/Library/DatePicker/DatePicker';
+import { FormButton } from '@/Library/FormButton/FormButton';
+import { SearchBar } from '@/Library/SearchBar/SearchBar';
+import { InputString } from '@/Library/InputString/InputString';
+import { InputNumber } from '@/Library/InputNumber/InputNumber';
+import { SelectMultiple } from '@/Library/SelectMultiple/SelectMultiple';
 // Hooks and utilities
-
-// Configuration
-import settings from '@/settings/settings';
-// Styles
-import './LandingPage.css';
-import { CustomizableButton } from '@/Library/CustomizableButton/CustomizableButton';
-import { Moon, Sun } from 'lucide-react';
+import { Controller, useForm } from 'react-hook-form';
 import { useBoundStore } from '@/app/store/useBoundStore';
-/* import { MagicCard } from '@/Library/MagicCard/MagicCard'; */
-import { MagicCard } from 'my-library';
+// Configuration & styles
+import './LandingPage.css';
+import { CustomizableButton } from '@/Library/CustomizableButton';
+import { Moon, Sun } from 'lucide-react';
 
-const IMGPATH = `/images/moi.jpg`;
-//
-const colorTheme1 = {
-	gradient: ['rgb(192, 38, 211)', 'rgb(219, 39, 119)'],
-	backGround: '#2b2b4b',
+const generateRandomIds = (count: number, length: number) =>
+	Array.from({ length: count }, () => nanoid(length));
+
+const randomIds = generateRandomIds(50, 8);
+const disabledStateTest = false;
+const regexString = /[^a-zA-Z0-9@.]/g;
+
+type TTestingForm = {
+	dateRange: [Date, Date];
+	multiple: string[];
+	searchBar: string;
+	myText: string;
+	myNumber: number;
 };
-const colorTheme2 = {
-	gradient: ['rgb(153, 246, 228)', 'rgb(217, 249, 157)'],
-	backGround: '#2b2b4b',
-};
-const colorTheme3 = {
-	gradient: ['rgb(59, 130, 246)', 'rgb(37, 99, 235)'],
-	backGround: '#2b2b4b',
-};
-const colorTheme4 = {
-	gradient: ['rgb(251, 113, 133)', 'rgb(253, 186, 116)'],
-	backGround: '#2b2b4b',
-};
-const sampling = 1000;
+
+const TestSchema: ZodType<TTestingForm> = z.object({
+	dateRange: z.tuple([z.date(), z.date()]),
+	multiple: z.array(z.string().toLowerCase().trim().min(1)).nonempty(),
+	searchBar: z.string().toLowerCase().trim().min(1),
+	myText: z.string().toLowerCase().trim().min(8),
+	myNumber: z.number().finite().safe().min(-20).max(20),
+});
 
 const LandingPage = () => {
-	/*  const [areaChartData, setAreaChartData] = useState({
-		timeStamp: 1,
-		convergence: 3000,
-		flux: 1398,
-		saturation: 2210, 
-	}); */
-	/* 	const [pieChartData, setPieChartData] = useState({
-		pieChart1: 10,
-		pieChart2: 80,
-		pieChart3: 10,
-		pieChart4: 10,
-	}); */
-	// Effect to
-	/* 	useEffect(() => {
-		let time = 0;
-		const interval = setInterval(() => {
-			time += 1;
-			const newData = {
-				timeStamp: time,
-				convergence: getRandomNbs(0, 3000),
-				flux: getRandomNbs(0, 3000),
-				saturation: getRandomNbs(0, 3000),
-			};
-
-			setAreaChartData(newData);
-		}, sampling);
-
-		return () => clearInterval(interval);
-	}, []); */
-
-	// Effect to
-	/* 	useEffect(() => {
-		const interval = setInterval(() => {
-			const newPieData = {
-				pieChart1: getRandomNbs(50, 100),
-				pieChart2: getRandomNbs(40, 60),
-				pieChart3: getRandomNbs(0, 100),
-				pieChart4: getRandomNbs(20, 90),
-			};
-			setPieChartData(newPieData);
-		}, sampling * 2);
-
-		return () => clearInterval(interval);
-	}, []); */
-
 	// Store
 	const toggleDarkMode = useBoundStore((state) => state.toggleDarkMode);
 
@@ -93,54 +51,145 @@ const LandingPage = () => {
 		toggleDarkMode();
 	};
 
+	const {
+		handleSubmit,
+		reset,
+		control,
+		formState: { isValid, isSubmitting, errors },
+	} = useForm<TTestingForm>({
+		mode: 'onChange',
+		resolver: zodResolver(TestSchema),
+	});
+
+	const submitData = async (data: TTestingForm) => {
+		console.log(data);
+		reset();
+	};
+
 	return (
-		<div className='super-container'>
-			<div className='container'>
-				<MagicCard isShadow={false} style='background'>
-					<ShowRoomForm></ShowRoomForm>
-				</MagicCard>
-			</div>
-			<CustomizableButton
-				disabled={false}
-				className={undefined}
-				handleClick={handleClick}
-			>
+		<form onSubmit={handleSubmit(submitData)} className={'container'}>
+			{/* ############################################## */}
+			{/* ############################################## */}
+
+			<CustomizableButton handleClick={handleClick}>
 				<Moon></Moon>
 				<Sun></Sun>
 			</CustomizableButton>
-			{/* <div className='container pie-wrapper'>
-				<PieChartEx
-					label={'Superposition ε'}
-					value={pieChartData.pieChart4}
-					color={colorTheme4}
-					sampling={sampling}
-					id='pieChart4'
-				/>
-				<PieChartEx
-					label={'Superposition α'}
-					value={pieChartData.pieChart1}
-					color={colorTheme1}
-					sampling={sampling}
-					id='pieChart1'
-				/>
-				<PieChartEx
-					label={'Superposition β'}
-					value={pieChartData.pieChart2}
-					color={colorTheme2}
-					sampling={sampling}
-					id='pieChart2'
-				/>
-			</div>
-			<div className='container area-chart'>
-				<AreaChartEx
-					newData={areaChartData}
-					label='Courbe n°1'
-					sampling={sampling}
-					width='100%'
-					height='100%'
-				></AreaChartEx>
-			</div> */}
-		</div>
+
+			{/* ############################################## */}
+			{/* ############################################## */}
+
+			<Controller
+				control={control}
+				name='dateRange'
+				render={({ field: { onChange } }) => (
+					<DatePicker
+						limitDateMin={-10}
+						limitDateMax={10}
+						placeholder='JJ/MM/AAAA'
+						isSubmit={isSubmitting}
+						isValid={isValid}
+						open={true}
+						disabled={disabledStateTest}
+						onFieldChange={onChange}
+						size='standard'
+						className='testDatePicker'
+					></DatePicker>
+				)}
+			/>
+
+			{/* ############################################## */}
+			{/* ############################################## */}
+			<Controller
+				control={control}
+				name='multiple'
+				render={({ field: { onChange } }) => (
+					<SelectMultiple
+						data={randomIds}
+						placeholder='Multiple'
+						size={31}
+						overSizeLimit={999}
+						isSubmit={isSubmitting}
+						isValid={isValid}
+						pickLimit={3}
+						disabled={disabledStateTest}
+						onFieldChange={onChange}
+					></SelectMultiple>
+				)}
+			/>
+
+			{/* ############################################## */}
+			{/* ############################################## */}
+			<Controller
+				control={control}
+				name='searchBar'
+				render={({ field: { onChange } }) => (
+					<SearchBar
+						data={randomIds}
+						size={31}
+						placeholder='Search'
+						overSizeLimit={999}
+						isSubmit={isSubmitting}
+						isValid={isValid}
+						disabled={disabledStateTest}
+						onFieldChange={onChange}
+					></SearchBar>
+				)}
+			/>
+
+			{/* ############################################## */}
+			{/* ############################################## */}
+			<Controller
+				control={control}
+				name='myText'
+				render={({ field: { onChange } }) => (
+					<InputString
+						type={'text'}
+						regex={regexString}
+						placeholder='My text here!'
+						errors={errors['myText']?.message}
+						size={31}
+						isSubmit={isSubmitting}
+						isValid={isValid}
+						disabled={disabledStateTest}
+						onFieldChange={onChange}
+					></InputString>
+				)}
+			/>
+
+			{/* ############################################## */}
+			{/* ############################################## */}
+
+			<Controller
+				control={control}
+				name='myNumber'
+				render={({ field: { onChange } }) => (
+					<InputNumber
+						placeholder='My number here!'
+						size={30}
+						errors={errors['myNumber']?.message}
+						step='0.1'
+						min='-20'
+						max='20'
+						outputNumber={true}
+						isSubmit={isSubmitting}
+						isValid={isValid}
+						disabled={disabledStateTest}
+						onFieldChange={onChange}
+					></InputNumber>
+				)}
+			/>
+
+			{/* ############################################## */}
+			{/* ############################################## */}
+
+			<FormButton
+				isValid={isValid}
+				isSubmit={isSubmitting}
+				disabled={disabledStateTest}
+				placeholder={'Validation'}
+			></FormButton>
+		</form>
 	);
 };
 export default LandingPage;
